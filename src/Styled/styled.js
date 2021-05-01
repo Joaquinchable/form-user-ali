e690ca127b36cd95fd402184efd81596d875f17b
Joaquin Chable <joaquinchable9@gmail.com>
2021-05-01 14:26:18 -0500

    Responsive

--- src/Styled/styled.js
+++ src/Styled/styled.js
@@ -12,6 +12,31 @@ export const ContainerBox = styled.div`
   width: 100%;
   height: 940px;
   background-color: black;
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    width: 100%;
+    height: 720px;
+    padding: 15px;
+  }
+
+  @media (max-width: 812px) {
+    width: 100%;
+    height: 720px;
+    padding: 2px;
+  }
+  @media (max-width: 1024px) {
+    width: 100%;
+    height: 880px;
+    padding: 15px;
+  }
+
+  @media (max-width: 1366px) {
+    width: 100%;
+    height: 880px;
+    padding: 15px;
+  }
 `;
 
 export const ChatBox = styled.div`
@@ -32,7 +57,34 @@ export const ChatBox = styled.div`
   border-radius: 32px;
   border-bottom-right-radius: 10px;
   border-bottom-left-radius: 10px;
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    width: 100%;
+    height: 670px;
+    padding: 2px;
+  }
+
+  @media (max-width: 712px) {
+    width: 100%;
+    height: 750px;
+  }
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    height: 840px;
+    padding: 2px;
+  }
+
+  @media (max-width: 1366px) {
+    width: 100%;
+    height: 840px;
+    padding: 2px;
+  }
 `;
+
+//   ConatinerAvatar
 
 export const ConatinerAvatar = styled.div`
   display: flex;
@@ -49,13 +101,96 @@ export const ConatinerAvatar = styled.div`
   border-bottom-right-radius: 10px;
   /* opacity: 0.7; */
   background-color: rgb(0, 0, 0);
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    width: 100%;
+    height: 100px;
+    padding: 8px;
+  }
+
+  @media (max-width: 812px) {
+    width: 100%;
+    height: 120px;
+    padding: 4px;
+    margin-bottom: 25px;
+  }
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    height: 120px;
+    margin-top: 20px;
+    padding: 8px;
+    margin-bottom: 25px;
+  }
+
+  @media (max-width: 1366px) {
+    width: 100%;
+    height: 190px;
+    margin-top: 5px;
+    padding: 8px;
+    margin-bottom: 25px;
+  }
 `;
+
 export const BoxImg = styled.img`
   width: 25%;
   margin-left: 100px;
   -webkit-border-radius: 100px;
   -moz-border-radius: 100px;
   border-radius: 100px;
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    width: 25%;
+    margin-left: 30px;
+  }
+
+  @media (max-width: 812px) {
+    width: 30%;
+    margin-left: 10px;
+  }
+
+  @media (max-width: 1024px) {
+    width: 25%;
+    margin-left: 140px;
+  }
+
+  /* @media (max-width: 1366px) {
+    width: 25%;
+    margin-left: 170px;
+  }  */
+`;
+
+export const Title = styled.h3`
+  color: #ffff;
+  font-size: 25px;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-top: 30px;
+  margin-left: 30px;
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    font-size: 20px;
+    margin-top: 20px;
+  }
+
+  @media (max-width: 812px) {
+    font-size: 20px;
+    margin-top: 20px;
+  }
+
+  @media (max-width: 1024px) {
+    font-size: 30px;
+    margin-top: 25px;
+  }
+  /* @media (max-width: 1366px) {
+    font-size: 40px;
+     margin-top: 55px;
+  }  */
 `;
 
 export const CardContainer = styled.article`
@@ -70,6 +205,24 @@ export const CardContainer = styled.article`
   -webkit-border-radius: 20px;
   -moz-border-radius: 20px;
   border-radius: 20px;
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    height: 400px;
+  }
+
+  @media (max-width: 712px) {
+    height: 500px;
+  }
+
+  @media (max-width: 1024px) {
+    height: 600px;
+  }
+
+  @media (max-width: 1366px) {
+    height: 700px;
+  }
 `;
 
 export const ContainerMessage = styled.div`
@@ -82,11 +235,10 @@ export const ContainerMessage = styled.div`
   -webkit-border-radius: 10px;
   -moz-border-radius: 10px;
   border-radius: 10px;
-`;
-export const Title = styled.h3`
-  color: #ffff;
-  font-size: 25px;
-  font-family: Arial, Helvetica, sans-serif;
-  margin-top:30px;
-  margin-left:30px;
+
+  /* Responsive */
+
+  @media (max-width: 667px) {
+    width: 100%;
+  }
 `;
